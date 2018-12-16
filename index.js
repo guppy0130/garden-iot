d245c6111dd5c4528cd861f9a12daa906934c33e
guppy0130 <guppy0130@yahoo.com>
2018-12-15 17:13:09 -0800

    case sensitive variables?

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const app = express();
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 
 let alerting = false;
 const grafanaStatuses = ['ok', 'paused', 'alerting', 'pending', 'no_data'];
